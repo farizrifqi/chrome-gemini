ecdfe6f3c018459b4b98990bcd382638e8af4dac
zeraneL <farizrifqi26@gmail.com>
2024-06-26 10:11:23 +0800

    with reload

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -20,15 +20,19 @@ const message: { [key: number]: string } = {
 
 export default function MainPage() {
   const [loaded, setLoaded] = useState(0 as number);
+  const [attempt, setAttempt] = useState(0 as number);
   const [aiSession, setAISession] = useState({} as any);
   useEffect(() => {
     const customWindow = window as CustomWindow;
-
     if (typeof window !== "undefined") {
       if (typeof customWindow.ai !== "undefined") {
         customWindow.ai.canCreateTextSession().then((res: any) => {
           if (res != "readily") {
-            setLoaded(2);
+            if (attempt < 3) {
+              setAttempt(attempt + 1);
+            } else {
+              setLoaded(2);
+            }
             return;
           }
           customWindow.ai.createTextSession().then((ses: any) => {
@@ -37,10 +41,14 @@ export default function MainPage() {
           });
         });
       } else {
-        setLoaded(3);
+        if (attempt < 3) {
+          setAttempt(attempt + 1);
+        } else {
+          setLoaded(3);
+        }
       }
     }
-  }, []);
+  }, [attempt]);
   return (
     <div className="h-full flex flex-col items-center p-10 gap-5 w-full max-w-screen-xl">
       {componentMapping[loaded]({ message: message[loaded], aiSession })}
